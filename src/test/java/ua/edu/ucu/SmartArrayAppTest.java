29f73b7719e5d977f8ef0f52050ab537e833db4c
Артемій <44300059+Katerunner@users.noreply.github.com>
2019-11-14 15:53:23 +0200

    new tests

--- src/test/java/ua/edu/ucu/SmartArrayAppTest.java
+++ src/test/java/ua/edu/ucu/SmartArrayAppTest.java
@@ -1,6 +1,11 @@
 package ua.edu.ucu;
 
+import org.junit.Before;
 import org.junit.Test;
+import ua.edu.ucu.functions.MyComparator;
+import ua.edu.ucu.functions.MyFunction;
+import ua.edu.ucu.functions.MyPredicate;
+import ua.edu.ucu.smartarr.*;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +13,16 @@ import static org.junit.Assert.*;
  * @author Andrii_Rodionov
  */
 public class SmartArrayAppTest {
+    private MyPredicate pr;
+    private MyComparator cmp;
+    private MyFunction func;
+
+    @Before
+    public void setUp() {
+        pr = t -> ((Integer) t) > 0;
+        cmp = (o1, o2) -> ((Integer) o1) - ((Integer) o2);
+        func = t -> 2 * ((Integer) t);
+    }
 
     @Test
     public void testFilterPositiveIntegersSortAndMultiplyBy2() {
@@ -43,4 +58,28 @@ public class SmartArrayAppTest {
         SmartArrayApp sma = new SmartArrayApp();
         assertEquals("ua.edu.ucu.SmartArrayApp", sma.getClass().getName());
     }
+
+    @Test
+    public void testDescription() {
+        BaseArray ba = new BaseArray(new Object[0]);
+        DistinctDecorator dd = new DistinctDecorator(ba);
+        FilterDecorator fd = new FilterDecorator(ba, pr);
+        SortDecorator sd = new SortDecorator(ba, cmp);
+        MapDecorator md = new MapDecorator(ba, func);
+
+
+        assertEquals("Base array", ba.operationDescription());
+        assertEquals("Distinct Decorator, removes duplicates",
+                dd.operationDescription());
+        assertEquals("Filter Decorator, tests every element and removes it " +
+                "if it doesn't satisfy MyPredicate", fd.operationDescription());
+        assertEquals("Sort Decorator, " +
+                        "sorts elements using MyComparator to compare them",
+                sd.operationDescription());
+        assertEquals("Map Decorator, maps every element to another object using MyFunction",
+                md.operationDescription());
+
+
+    }
+
 }
